34bfc4459d146db9592b0ded909783cdb0909be9
Jino1211 <yone543@gmail.com>
2021-01-27 19:15:02 +0200

    fixed function
    add function to grab the list in array
    split big function to few.
    add sorf button still without js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 const viewSection = document.querySelector('#view-section');
 const controlSection = document.querySelector('#control-section');
 let clicksParagraph = document.querySelector('#counter-paragraph span');
+let todoContainer;
 let clicks = 0;
+let detailsInput = {};
 console.log(clicksParagraph);
 // const ulHolderItems = document.createElement('ul');
 // ulHolderItems.classList.add('items-list');
@@ -9,35 +11,45 @@ console.log(clicksParagraph);
 
 const addForm = document.forms['add-form']; 
 
-//when click on button that function grab the input and the time and send it to => 'addItem' and then clear the input field 
+//when click on button that function grab the inputs and the time and send it to the right function.
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const valueInput = addForm.querySelector('input[type="text"]').value;
     const valuePriority = addForm.querySelector('select').value;
     let timeCreated = new Date();
     timeCreated = timeCreated.toLocaleString();
-    addItems(valueInput, valuePriority, timeCreated);
-    addForm.querySelector('input[type="text"]').value = "";
+    addTodoContainer();
+    addInput(valueInput);
+    addDate(timeCreated);
+    addPriority(valuePriority);
 })
 
-//get the time when created input values from users and added them to the list.
-function addItems(valueInput, valuePriority, timeCreated) {
-    const todoContainer = document.createElement('div');
+//build the container div for the inputs
+function addTodoContainer() {
+    todoContainer = document.createElement('div');
     todoContainer.classList.add('todo-container')
     viewSection.prepend(todoContainer);
-    const todoPriority = document.createElement('div');
-    todoPriority.classList.add('todo-priority');
-    todoContainer.append(todoPriority);
+}
+//get the value and add it to the list
+function addInput (valueInput) {
     const todoText = document.createElement('div');
     todoText.classList.add('todo-text');
     todoContainer.append(todoText);
+    todoText.innerText = valueInput;
+}
+//get the date and add it to the list
+function addDate (timeCreated) {
     const todoTime= document.createElement('div');
     todoTime.classList.add('todo-created-at');
     todoContainer.append(todoTime);
-    
-    todoText.innerText = valueInput;
-    todoPriority.innerText = valuePriority;
     todoTime.innerText = timeCreated;
+}
+//get the priority  and add it to the list
+function addPriority (valuePriority) {
+    const todoPriority = document.createElement('div');
+    todoPriority.classList.add('todo-priority');
+    todoContainer.append(todoPriority);
+    todoPriority.innerText = valuePriority;
 }
 
 addForm.addEventListener('submit', (e) => {
@@ -48,8 +60,24 @@ addForm.addEventListener('submit', (e) => {
         clicksParagraph.innerText = `${clicks} task`;
     }
 })
-// const json = JSON.stringify(viewSection);
-// console.log(json);
+
+addForm.addEventListener('submit', () => {
+    const valueInput = addForm.querySelector('input[type="text"]').value;
+    const valuePriority = addForm.querySelector('select').value;
+    let timeCreated = new Date();
+    makeObject(valueInput, timeCreated, valuePriority, clicks)
+    addForm.querySelector('input[type="text"]').value = "";
+});
+
+let mainArrayContainer = [];
+function makeObject(valueInput, timeCreated, valuePriority, clicks) {
+    detailsInput.text = valueInput;
+    detailsInput.date = timeCreated;
+    detailsInput.priority = valuePriority;
+    arr.push(detailsInput)
+    let json = JSON.stringify(arr);
+}
+
 
 
 
